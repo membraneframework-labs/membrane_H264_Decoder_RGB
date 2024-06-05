2dd1f427b008cec2cd03cc3ad066c38651219239
Bartek Chaliński <b.chalinski@gmail.com>
2024-06-05 15:12:00 +0200

    added video scaling

--- lib/membrane_h264_decoder_rgb_bin.ex
+++ lib/membrane_h264_decoder_rgb_bin.ex
@@ -1,10 +1,11 @@
 defmodule Membrane.H264_RGB.Decoder do
   @moduledoc """
-  This bin takes H264 video, decodes into raw video and converts pixel format to RGB.
+  This bin takes H264 video, decodes into raw video, scales to desired resolution and converts pixel format to RGB.
   """
   use Membrane.Bin
 
   alias Membrane.FFmpeg.SWScale.PixelFormatConverter, as: PixelFormatConverter
+  alias Membrane.FFmpeg.SWScale.Scaler, as: Scaler
   alias Membrane.H264
   alias Membrane.RawVideo
 
@@ -16,14 +17,27 @@ defmodule Membrane.H264_RGB.Decoder do
     accepted_format: RawVideo,
     availability: :always
 
+    def_options resolution: [
+      spec: %{width: non_neg_integer(), height: non_neg_integer()} | :native,
+      default: :native,
+      description:
+        "Desired output resolution."
+    ]
+
   @impl true
-  def handle_init(_ctx, _options) do
-    structure = [
+  def handle_init(_ctx, options) do
+    structure = if options.resolution == :native do
       bin_input()
       |> child(:decoder, Membrane.H264.FFmpeg.Decoder)
       |> child(:converter, %PixelFormatConverter{format: :RGB})
       |> bin_output()
-    ]
+    else
+      bin_input()
+      |> child(:decoder, Membrane.H264.FFmpeg.Decoder)
+      |> child(:scaler, %Scaler{output_width: options.resolution.width, output_height: options.resolution.height})
+      |> child(:converter, %PixelFormatConverter{format: :RGB})
+      |> bin_output()
+    end
 
     {[spec: structure], %{}}
   end
